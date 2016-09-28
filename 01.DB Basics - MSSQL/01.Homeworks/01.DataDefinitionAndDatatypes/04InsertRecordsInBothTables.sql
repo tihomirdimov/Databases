604d35eb0a4a6a3a3b6f5616a5597723ecf69d3e
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-09-28 11:19:41 +0300

    DB HW 01 Problem 04

--- 01.DB Basics - MSSQL/01.Homeworks/01.DataDefinitionAndDatatypes/04InsertRecordsInBothTables.sql
+++ 01.DB Basics - MSSQL/01.Homeworks/01.DataDefinitionAndDatatypes/04InsertRecordsInBothTables.sql
@@ -1,13 +1,12 @@
-INSERT INTO Minions
-    (Id, Name,Age,TownId)
-VALUES
-    (1 , 'Kevin', 15,1),
-    (2 , 'Bob', 22,3),
-    (3 , 'Steward', NULL, 2);
-
-	INSERT INTO Towns
+INSERT INTO Towns
     (Id, Name)
 VALUES
     (1 , 'Sofia'),
     (2 , 'Plovdiv'),
     (3 , 'Varna');
+INSERT INTO Minions
+    (Id, Name,Age,TownId)
+VALUES
+    (1 , 'Kevin', 22,1),
+    (2 , 'Bob', 15,3),
+    (3 , 'Steward', NULL, 2);
