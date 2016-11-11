dd815e7f0d6604e6fe3b847d7df693ae27e3596b
tihomirdimov <tihomirdimov@tihomirdimov.com>
2016-11-11 23:50:54 +0200

    first commit DB apps intro

--- 02.DB Advanced - MSSQL/01.Homeworks/01IntroductionToDBApps01/InitialSetup.cs
+++ 02.DB Advanced - MSSQL/01.Homeworks/01IntroductionToDBApps01/InitialSetup.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01.InitialSetup
+namespace _01IntroductionToDBApps01
 {
-    class Program
+    class InitialSetup
     {
         static void Main(string[] args)
         {
-            int test = -5;
-            Console.WriteLine(test);
-            Console.WriteLine(test);
         }
     }
 }
--- 02.DB Advanced - MSSQL/01.Homeworks/01IntroductionToDBApps01/Properties/AssemblyInfo.cs
+++ 02.DB Advanced - MSSQL/01.Homeworks/01IntroductionToDBApps01/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("01.InitialSetup")]
+[assembly: AssemblyTitle("01IntroductionToDBApps01")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("01.InitialSetup")]
+[assembly: AssemblyProduct("01IntroductionToDBApps01")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("452081e9-b9a1-49df-83f6-6344271203bf")]
+[assembly: Guid("91cc6dd5-5bd1-4397-b50f-b3a858ee3e8b")]
 
 // Version information for an assembly consists of the following four values:
 //
